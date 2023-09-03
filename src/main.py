007e0c4ca8b46a20b7b7eb28bf8b6a869c4944ae
Victor Chiaka <chiakavictor205@gmail.com>
2023-09-03 15:11:24 +0100

    add: added tailwind

---
+++ src/main.py
@@ -0,0 +1,3 @@
+from webapp import initialize_app
+
+app = initialize_app()
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/templates/*.{html}", "./src/static/*.{js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
