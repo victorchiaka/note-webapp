fffe7818a6652276b54a9fe65990b27a7fdeeca0
Victor Chiaka <chiakavictor205@gmail.com>
2023-09-04 17:21:52 +0100

    feat: implemented better nav

---
+++ src/webapp/static/js/main.js
@@ -0,0 +1,8 @@
+const mobileNavbar = document.getElementById("mobile-nav");
+
+const hamburgerToggler = document.getElementById("hamburger-toggler");
+
+hamburgerToggler.addEventListener("click", () => {
+    hamburgerToggler.classList.toggle("open-toggle");
+    mobileNavbar.classList.toggle("open-mobile-nav");
+});
