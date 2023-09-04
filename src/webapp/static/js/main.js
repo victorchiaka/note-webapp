2cd5606d1bda3cb3055c84e7e82041d07f311a1a
Victor Chiaka <chiakavictor205@gmail.com>
2023-09-04 21:23:19 +0100

    fix: mobile hamburger menu

--- src/webapp/static/js/main.js
+++ src/webapp/static/js/main.js
@@ -3,6 +3,6 @@ const mobileNavbar = document.getElementById("mobile-nav");
 const hamburgerToggler = document.getElementById("hamburger-toggler");
 
 hamburgerToggler.addEventListener("click", () => {
-    hamburgerToggler.classList.toggle("open-toggle");
-    mobileNavbar.classList.toggle("open-mobile-nav");
+  hamburgerToggler.classList.toggle("open-toggle");
+  mobileNavbar.classList.toggle("open-mobile-nav");
 });
