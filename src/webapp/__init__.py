981f3970714a2f5fbbe2dbbb99bbe8b94d5279c0
Victor Chiaka <chiakavictor205@gmail.com>
2023-09-05 16:02:14 +0100

    desing signup form

--- src/webapp/__init__.py
+++ src/webapp/__init__.py
@@ -22,6 +22,6 @@ def initialize_app():
         
     # registeres the blueprint
     app.register_blueprint(views, url_prefix="/")
-    app.register_blueprint(auth, url_prefix="/auth/")
+    app.register_blueprint(auth, url_prefix="/")
     
     return app
