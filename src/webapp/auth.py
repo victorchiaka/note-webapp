1a5fb5b6f7d2c22313cb1554c0e648e3f7253866
Victor Chiaka <chiakavictor205@gmail.com>
2023-09-05 16:52:23 +0100

    getting users data from form

--- src/webapp/auth.py
+++ src/webapp/auth.py
@@ -1,9 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/signup")
+@auth.route("/signup", methods=["GET", "POST"])
 def signup():
+    if request.method == "POST":
+        data = request.form
+        username = data.get("username")
+        email = data.get("email")
+        password = data.get("password")
+        confirm_password = data.get("confirm-password")
+        
+
     return render_template("signup.html")
 
 @auth.route("/login")
