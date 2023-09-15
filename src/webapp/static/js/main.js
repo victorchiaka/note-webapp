a455f741a905102eb5e7ad62a0dc080c868a4e8b
Victor Chiaka <chiakavictor205@gmail.com>
2023-09-15 19:11:06 +0100

    designed home

--- src/webapp/static/js/main.js
+++ src/webapp/static/js/main.js
@@ -1,10 +1,17 @@
 const mobileNavbar = document.getElementById("mobile-nav");
-
 const hamburgerToggler = document.getElementById("hamburger-toggler");
 
 hamburgerToggler.addEventListener("click", () => {
   hamburgerToggler.classList.toggle("open-toggle");
   mobileNavbar.classList.toggle("open-mobile-nav");
+});
+
+const threeDotsNav = document.getElementById("three-dots-nav");
+const threeDotsNavToggler = document.getElementById("three-dots-toggle");
+
+threeDotsNavToggler.addEventListener("click", () => {
+  threeDotsNav.classList.toggle("flex");
+  threeDotsNav.classList.toggle("hidden");
 });
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -29,5 +36,4 @@ document.addEventListener("DOMContentLoaded", function () {
   }, 3000);
   document.addEventListener("DOMContentLoaded", function () {
   });
-  
 });
