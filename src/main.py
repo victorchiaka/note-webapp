2996775c1b326c649b66e27808064fd294634b25
Victor Chiaka <chiakavictor205@gmail.com>
2023-09-18 15:52:18 +0100

    removed debug

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ app = initialize_app()
 
 if IS_DEVELOPMENT:
     if __name__ == "__main__":
-        app.run(debug=True)
+        app.run(debug=False)
 else:
     if __name__ == "__main__":
         app.run()
